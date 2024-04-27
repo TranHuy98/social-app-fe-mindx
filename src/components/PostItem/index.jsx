fe0588206dfcffa689a7bd3882534143a079a18f
TranHuy98 <huy8663@gmail.com>
2024-04-27 16:39:09 +0700

    test connect api

--- src/components/PostItem/index.jsx
+++ src/components/PostItem/index.jsx
@@ -15,9 +15,28 @@ const PostItem = (props) => {
     const getContent = `${checkingContentLength ? `${templateContent.slice(0, 150)}...` : templateContent}`;
     const [viewMore, setViewMore] = useState(false);
 
+    // moi them
+    const [noidung, setNoiDung] = useState();
+
     const handleViewMore = (view) => {
         setViewMore(view)
     };
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const response = await fetch('https://project-social-app-mindx77.onrender.com/posts/662644ae7584eca051681af1', {
+                    method: GET,
+                });
+                const data = await response.json();
+                setNoiDung(data)
+            } catch (error) {
+                console.error('Failed to fetch users', error);
+            }
+        };
+
+        fetchPost();
+    }, []);
 
     return (
         <div className={`post-item bg-white ${props.className}`}>
@@ -34,7 +53,7 @@ const PostItem = (props) => {
             </div>
             <div className={"content-post"}>
                 <p>
-                    {viewMore ? templateContent : getContent} {checkingContentLength &&
+                    {viewMore ? noidung.postContent : getContent} {checkingContentLength &&
                         <span
                             className="view-more"
                             onClick={() => {
