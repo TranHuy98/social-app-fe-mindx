054ec75feda32c8b76d6eabd137c5741dac2cff5
TranHuy98 <huy8663@gmail.com>
2024-04-30 23:25:32 +0700

    test home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CreatePost from '../../components/CreatePost';
 import PostItem from '../../components/PostItem';
 import './styles.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 //30/4/2024
 
@@ -12,18 +12,23 @@ const Home = () => {
 
     const [postData, setPostData] = useState();
 
-    axios.get('https://project-social-app-mindx77.onrender.com/posts')
-        .then(response => {
+    useEffect(() => {
+        axios.get('https://project-social-app-mindx77.onrender.com/posts')
+            .then(response => {
 
-            console.log(response.data.data);
-            console.log('test api success!');
-            setPostData(response.data.data.items);
-            console.log(postData);
-        })
-        .catch(error => {
-            // Xử lý lỗi ở đây
-            console.error(error);
-        });
+                console.log("data tra ve: " + response.data.data);
+                console.log('test api success!');
+                setPostData(response.data.data.items);
+                console.log("postData " + postData);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        return () => {
+
+        };
+    }, []);
 
     return (
         <div className="container-home">
