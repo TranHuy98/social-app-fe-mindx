017de22a01127af2401551be9fb12ce7ad46a9e0
TranHuy98 <huy8663@gmail.com>
2024-05-01 16:18:41 +0700

    test render post

--- src/components/PostItem/index.jsx
+++ src/components/PostItem/index.jsx
@@ -12,10 +12,11 @@ import { useEffect } from 'react';
 
 // const templateContent = "Lorem Ipsum";
 
-const PostItem = ({post, key}) => {
+const PostItem = (post, key) => {
     const templateContent = post;
     console.log('du lieu post item');
     console.log(JSON.stringify(post, null, 2));
+    console.log(JSON.parse(post));
     console.log(templateContent);
     console.log(typeof (templateContent));
 
